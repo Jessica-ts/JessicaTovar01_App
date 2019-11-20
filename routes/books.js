2167ecf00fa794dfa976fa1cdc52ccb869e25672
Jessica Tovar <jessica_tovar_s@hotmail.com>
2019-11-19 18:51:41 -0600

    Image

--- routes/books.js
+++ routes/books.js
@@ -121,7 +121,11 @@ router.get('/books/edit/:id',isAuthenticated, async (req, res) => {
 
 router.put('/books/edit-books/:id', isAuthenticated, async (req,res) => 
 {
-	const {title, author, description, price, store, filename} = req.body;
+	const {title, author, description, price, store} = req.body;
+	if(filename)
+	{
+		filename=req.body;
+	}
 	await Book.findByIdAndUpdate(req.params.id, {title, author, description, price, store, filename});
 	req.flash('success_msg', 'Note Update successfuly');
 	res.redirect('/books');

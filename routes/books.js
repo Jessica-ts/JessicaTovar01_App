6cb3d874f2fbfeb4e307e5a270dd578ce2215bc5
Jessica Tovar <jessica_tovar_s@hotmail.com>
2019-11-19 19:33:11 -0600

    Image verif if

--- routes/books.js
+++ routes/books.js
@@ -124,11 +124,12 @@ router.put('/books/edit-books/:id', isAuthenticated, async (req,res) =>
 	const {title, author, description, price, store, filename} = req.body;
 
 	
-	if(filename!="")
+	if(filename=="")
 	{
-		await Book.findByIdAndUpdate(req.params.id, {title, author, description, price, store, filename});
+		await Book.findByIdAndUpdate(req.params.id, {title, author, description, price, store});
 	}
-	await Book.findByIdAndUpdate(req.params.id, {title, author, description, price, store});
+	else
+		await Book.findByIdAndUpdate(req.params.id, {title, author, description, price, store, filename});
 	req.flash('success_msg', 'Note Update successfuly');
 	res.redirect('/books');
 });
